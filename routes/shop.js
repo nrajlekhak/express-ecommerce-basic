ad03ea406f777af4f31ef1e1816bbb8295d0245e
Niraj Lekhak <nrajlekhak@gmail.com>
2020-06-07 00:59:54 +0545

    hbs

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,12 @@
-const path = require('path');
-
 const express = require('express');
-
-const rootDir = require('../util/path');
 
 const adminData = require('../routes/admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', { pageTitle: 'My shop', products: products, path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true });
 });
 
 
