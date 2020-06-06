49e8630faa9b9037690730c1724abdcffc3a2dff
Niraj Lekhak <nrajlekhak@gmail.com>
2020-06-07 01:33:34 +0545

    module 6 final

--- routes/admin.js
+++ routes/admin.js
@@ -11,12 +11,11 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
     res.render('add-product', {
         pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true,
+        path: '/admin/add-product'
+
     });
 });
+
 
 router.post('/add-product', (req, res, next) => {
     products.push({ title: req.body.title });
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', { pageTitle: 'My shop', products: products, path: '/', hasProducts: products.length > 0, activeShop: true, productCSS: true });
+    res.render('shop', {
+        pageTitle: 'My shop',
+        products: products,
+        path: '/'
+    });
 });
 
 
